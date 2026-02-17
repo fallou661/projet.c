0e0a84039b097b756d965a136c74199595b807d5
fallou661 <fallou_2013@icloud.com>
2026-02-17 16:31:27 +0000

    moyenne impair

--- exoversionning.c
+++ exoversionning.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main (){
-    int n,x,i,moyenne,somme,cpt;
+    int n,x,i,moyenne,somme,cpt,somme1,moyenne1;
     do{
        printf("saisir n entiers : ");
        scanf("%d",&n);
@@ -19,6 +19,8 @@ int main (){
        }
     }
      moyenne=somme/cpt;
+     moyenne1=somme1/n-cpt;
       printf("La moyenne des nombres pairs est %d",moyenne);
+      printf("La moyenne des nombres impairs est : %d",moyenne1);
 return 0;
 }
